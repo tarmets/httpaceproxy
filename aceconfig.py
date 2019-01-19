5761ebc1ec09e6ce3468f1ddc75cf4ac2d21f5e9
tarmets <dadacable@gmail.com>
2019-01-19 17:06:45 +0200

    Update aceconfig.py

--- aceconfig.py
+++ aceconfig.py
@@ -39,7 +39,7 @@ class AceConfig(acedefconfig.AceDefConfig):
     # ----------------------------------------------------
     # Since version 3.1.x is implemented HTTP API
     # Useed AceStream API method False - Engine API, True - HTTP API
-    new_api = False
+    new_api = True
     # For HLS transcode options is avalible:
     # Transcode All audio to AAC (transcode_audio=1)
     # Transcode MP3 (use only when transcode_audio=1)
