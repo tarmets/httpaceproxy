762b3de8ebbfde57da3f0db55c11a924852b3186
Tarmo <dadacable@gmail.com>
2019-01-25 21:28:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 /usr/bin/python3.7 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
 while true; do
-        sleep $1
+        sleep 600
         rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
 done
