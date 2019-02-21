abe33dc708bafc79dfffcd4c2bd395629e1876b2
tarmets <tarmosiin@gmail.com>
2019-02-22 00:21:02 +0200

    Update torrenttv.py

--- add/torrenttv.py
+++ add/torrenttv.py
@@ -18,7 +18,7 @@ url = 'http://91.92.66.82/trash/ttv-list/ttv.m3u'
 updateevery = 120
 
 # TV Guide URL
-tvgurl = 'https://iptvx.one/epg/epg.xml.gz'
+tvgurl = 'http://epg.do.am/tv.gz'
 
 # Shift the TV Guide time to the specified number of hours
 tvgshift = 0
@@ -31,5 +31,5 @@ tvgshift = 0
 # tvgid - channel tvg-id (optional)
 # group - channel playlist group-title (optional)
 # logo - channel picon file tvg-logo (optional)
-m3uheadertemplate = '#EXTM3U url-tvg="%s" tvg-shift=%d deinterlace=1 m3uautoload=1 cache=1000\n' % (tvgurl, tvgshift)
-m3uchanneltemplate = '#EXTINF:-1 group-title="%(group)s" tvg-name="%(tvg)s" tvg-logo="%(logo)s",%(name)s\n#EXTGRP:%(group)s\n%(url)s\n'
+m3uheadertemplate = u'#EXTM3U url-tvg="{}" tvg-shift={} deinterlace=1 m3uautoload=1 cache=1000\n'.format(tvgurl, tvgshift)
+m3uchanneltemplate = u'#EXTINF:-1 group-title="{group}" tvg-name="{tvg}" tvg-logo="{logo}",{name}\n#EXTGRP:{group}\n{url}\n'
