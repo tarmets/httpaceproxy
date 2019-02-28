512886a83c7c14a9c6deaae39ebf2949e1eef396
tarmets <tarmosiin@gmail.com>
2019-02-28 12:21:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
 while true; do
-        sleep 600
+        sleep 1200
         rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
 done
