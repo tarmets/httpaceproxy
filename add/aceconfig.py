282ae277a4be9e7907e0e9ef8e10022d3c1d6983
tarmets <tarmosiin@gmail.com>
2019-04-13 15:18:26 +0300

    Update aceconfig.py

--- add/aceconfig.py
+++ add/aceconfig.py
@@ -101,7 +101,7 @@ class AceConfig(acedefconfig.AceDefConfig):
     #transcodecmd['100k'] = 'ffmpeg -i - -c:a copy -b 100k -f mpegts -'.split()
     #transcodecmd['mp2'] = 'ffmpeg -i - -c:a mp2 -c:v mpeg2video -f mpegts -qscale:v 2 -'.split()
     #transcodecmd['mkv'] = 'ffmpeg -i - -map 0 -c:v copy -c:a copy -f matroska -'.split()
-    #transcodecmd['default'] = 'ffmpeg -i - -map 0 -c:a copy -c:v copy -f mpegts -'.split()
+    transcodecmd['default'] = 'ffmpeg -i - -map 0 -c:a copy -c:v copy -f mpegts -'.split()
     #-----------------------------------------------------
     # Using VLC (if cvlc is not installed use 'vlc -I dummy' instead)
     #-----------------------------------------------------
@@ -142,8 +142,7 @@ class AceConfig(acedefconfig.AceDefConfig):
         if not useragent:
             return False
         elif useragent in AceConfig.fakeuas:
-            return True
-       # Samsung ES series
+            return True       # Samsung ES series
         elif useragent == 'Lavf/55.33.100' and headers.get('Range') != 'bytes=0-':
             return True
         # Samsung H series
