97b0760c4c18bdfc6de101767080cb797945ccb2
tarmets <dadacable@gmail.com>
2019-09-10 00:39:08 +0300

    Update start.sh

--- add/start.sh
+++ add/start.sh
@@ -3,6 +3,6 @@
 python3 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
 while true; do
-        sleep 1200
+        sleep 600
         rm -rf /tmp/.ACEStream/collected_torrent_files/*
 done
